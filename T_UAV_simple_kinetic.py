4b0515638a0d6324f23070df66e99439280fa561
Bryan Guevara <bryansgue@gmail.com>
2023-10-30 18:46:21 -0300

    Test Marcos 2

--- T_UAV_simple_kinetic.py
+++ T_UAV_simple_kinetic.py
@@ -50,7 +50,7 @@ def main(vel_pub, vel_msg ):
    
     
     #TAREA DESEADA
-    value = 8
+    value = 9
     xd = lambda t: 4 * np.sin(value*0.04*t) + 3
     yd = lambda t: 4 * np.sin(value*0.08*t)
     zd = lambda t: 2 * np.sin(value*0.08*t) + 6
